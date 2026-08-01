207925d150780804676f483863ac2d55bcf4b5ac
Kaysha Motiani <kayshamotiani2007@gmail.com>
2026-08-01 23:03:45 +0530

    ✅ Set Matrix Zeroes | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 20.9 MB | 2026-08-01

--- leetcode/medium/0073-set-matrix-zeroes/solution.cpp
+++ leetcode/medium/0073-set-matrix-zeroes/solution.cpp
@@ -3,27 +3,40 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-
-        vector<bool> row(m , false);
-        vector<bool> col(n , false);
+        int col0 = 1;
         
         for (int i=0 ; i<m ; i++) {
-            for (int j=0 ; j<n ; j++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j=1 ; j<n ; j++) {
                 if ( matrix[i][j] == 0) {
-                   row[i] = true;
-                   col[j] = true;
+                   matrix[i][0] = 0;
+                   matrix[0][j] = 0;
                 }
             }
         }
 
-        for ( int i=0 ; i<m ; i++) {
-            for (int j=0 ; j<n ; j++) {
-                if ( row[i] || col[j]) {
+        for ( int i=1 ; i<m ; i++) {
+            for (int j=1 ; j<n ; j++) {
+                if ( matrix[i][0]==0 || matrix[0][j]==0) {
                     matrix[i][j] = 0;
+
                 }
 
             }
         }
+
+        if ( matrix[0][0] == 0) {
+            for ( int j=0 ; j<n ; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if ( col0 == 0) {
+            for (int i=0 ; i<m ; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        
 
 
         
