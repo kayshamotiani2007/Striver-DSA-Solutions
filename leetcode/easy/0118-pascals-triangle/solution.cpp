6a27455749a8963d4daf62a6c5c928219c8cd945
Kaysha Motiani <kayshamotiani2007@gmail.com>
2026-08-03 21:35:55 +0530

    ✅ Pascal's Triangle | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 9.6 MB | 2026-08-03

---
+++ leetcode/easy/0118-pascals-triangle/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+
+        for (int i = 0; i < numRows; i++) {
+            // Row 'i' has (i + 1) elements, initialized to 1
+            vector<int> row(i + 1, 1);
+
+            // Fill in the middle values using the previous row
+            for (int j = 1; j < i; j++) {
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+
+            triangle.push_back(row);
+        }
+
+        return triangle;
+    }
+};
